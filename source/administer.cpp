091f6d54424dbe51a03d0bd37041193b619cd92a
Sodataco <2284242286@qq.com>
2024-07-10 20:50:28 +0800

    Update administer.cpp

--- source/administer.cpp
+++ source/administer.cpp
@@ -84,21 +84,6 @@ bool Administer::derive_data_to_sql(QSqlDatabase& db){
 
 
 
-void Administer::on_importExcel_clicked()
-{
-    //实现获取文件路径的功能
-    QString filePath = QFileDialog::getOpenFileName(nullptr, "选择文件", QDir::homePath(), "Excel 文件 (*.xlsx *.xls)");
-
-    if(!filePath.isEmpty()){
-        this->readAndStoreExcelData(filePath,user_db);
-
-        QMessageBox::about(this, "棒", "Successfully import Excel.");
-    }
-    else{
-        QMessageBox::critical(this, "Damn", "Failed to import Excel.");
-    }
-
-}
 
 
 
